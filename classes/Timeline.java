e9c533489c3ba2c81dbfb90c1511c96a36c11c7f
connerv <conner.vick@my.wheaton.edu>
2014-02-13 14:15:44 -0600

    Update Timeline.java

--- classes/Timeline.java
+++ classes/Timeline.java
@@ -1,3 +1,5 @@
+package classes;
+
 import java.util.*;
 
 
@@ -6,8 +8,8 @@ public class Timeline {
 	//TreeMap datastructure according to ocs.oracle.com/javase/6/docs/api/java/util/TreeMap.html
 	//is a sorted hashmap essentially by key value that is comparable
 	// we can store the start dates as the key and the events as the value so its sorted by start date
-	
-	TreeMap<String, Event> timelineEvents = new TreeMap<String, Event>(); 
+
+	TreeMap<Integer, Event> timelineEvents = new TreeMap<Integer, Event>(); 
 
 	public Timeline(String name){
 		timelineEvents = null;
@@ -15,14 +17,14 @@ public class Timeline {
 
 	//add timeline event
 	public void addEvent(Event e){
-		timelineEvents.put(e.getStartDate(), e);
+		timelineEvents.put(e.getID(), e);
 	}
 
 	//checks if start date and name are same, then removes event
 	//NOTE, returns String to output to console depending on success or not
 	public String removeEvent(Event e){
-		if(timelineEvents.containsKey(e.getStartDate()) && e.getName()== timelineEvents.get(e.getStartDate()).getName()){
-			timelineEvents.remove(e.getStartDate());
+		if(timelineEvents.containsKey(e.getID()) && e.getName().equals(timelineEvents.get(e.getID()).getName())){
+			timelineEvents.remove(e.getID());
 			return e.getName() + " successfuly removed."; 
 		}else
 			return "Event not found in timeline.";
@@ -31,11 +33,11 @@ public class Timeline {
 	public void display(){
 		//need to implement this still
 	}
-	
+
 	public void loadTimeline(){
 		//need to implement this to take all the file info and load it into the timeline TreeMap 
 	}
-	
+
 	public void saveTimeline(){
 		//need to implement to save timeline as TreeMap to file.
 	}
